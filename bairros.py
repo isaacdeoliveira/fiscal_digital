d260d9d0de6da505c5a521d409076544a3a90002
isaacdeoliveira <isaacnunesdeoliveira@gmail.com>
2023-01-13 11:09:28 -0300

    ok

--- bairros.py
+++ bairros.py
@@ -13,36 +13,105 @@ import webbrowser
 conexao = psycopg2.connect(host='faus.arapiraca.al.gov.br', port = '5432', database='db_faus',
 user='fiscal', password='db#f1sc@l@R@19')
 cursor = conexao.cursor()  
-sql = 'SELECT COUNT(*) FROM public.termo_auto_infracao WHERE bairro_infracao_auto_infracao  ILIKE' +"'olho%'"
-cursor.execute(sql)
-conexao.commit()
-bairros = cursor.fetchall()
-
 bairros_chave = pd.read_csv("C:/Users/EquipeDev/Documents/bairros_chaves.csv", encoding='latin1')
+geoJSON = gpd.read_file('C:/Users/EquipeDev/Desktop/faus_holiday/produtividade/fiscal_digital/bairros_arapiraca.geojson')
 
 auto_infracao = []
 
-
+###################### Auto de Infração ############################################################################
 for i in bairros_chave["chave"]:
     sql = 'SELECT COUNT(*) FROM public.termo_auto_infracao WHERE bairro_infracao_auto_infracao  ILIKE '+"'" + i +"'"
     cursor.execute(sql)
     conexao.commit()
     qtd_auto_infracao = cursor.fetchall()
     auto_infracao.append(qtd_auto_infracao[0][0])
+bairros_chave["auto_infracao"] = auto_infracao
+auto_geojson = []
+for i in geoJSON.BAIRRO:
+    valor_auto = bairros_chave[bairros_chave.Bairro== i]["auto_infracao"].values[0]
+    auto_geojson.append(valor_auto)
+    
+geoJSON["auto_infracao"] = auto_geojson
+
+
+
+
+
+
+map_auto = folium.Map(location=[-9.751969332753832, -36.656550027706835], zoom_start=12,  tiles='OpenStreetMap')
+
+# Set up Choropleth map
+folium.Choropleth(
+geo_data=geoJSON,
+data=geoJSON,
+columns=['BAIRRO',"auto_infracao"],
+key_on="feature.properties.BAIRRO",
+fill_color='YlGnBu',
+fill_opacity=1,
+line_opacity=0.5,
+legend_name="Quantidade de Auto de Infração",
+smooth_factor=0,
+Highlight= True,
+line_color = "#0000",
+name = "Quantidade de Auto de Infração",
+show=False,
+overlay=True,
+nan_fill_color = "White"
+).add_to(map_auto)
+
+
+
+map_auto.save("map_auto.html")
+webbrowser.open("map_auto.html")
+########################## Notificação ##############################################################################
+
+notificacao = [] 
+
+for i in bairros_chave["chave"]:
+    sql = 'SELECT COUNT(*) FROM public.termo_notificacao WHERE bairro_infracao_notificacao  ILIKE '+"'" + i +"'"
+    cursor.execute(sql)
+    conexao.commit()
+    qtd_notificacao = cursor.fetchall()
+    notificacao.append(qtd_notificacao[0][0])
+bairros_chave["notificacao"] = notificacao
+notificacao_geojson = []
+for i in geoJSON.BAIRRO:
+    valor_notificacao = bairros_chave[bairros_chave.Bairro== i]["notificacao"].values[0]
+    notificacao_geojson.append(valor_notificacao)
+    
+geoJSON["notificacao"] = notificacao_geojson
+
+map_notificacao = folium.Map(location=[-9.751969332753832, -36.656550027706835], zoom_start=12,  tiles='OpenStreetMap')
+
+# Set up Choropleth map
+folium.Choropleth(
+geo_data=geoJSON,
+data=geoJSON,
+columns=['BAIRRO',"notificacao"],
+key_on="feature.properties.BAIRRO",
+fill_color='YlGnBu',
+fill_opacity=1,
+line_opacity=0.5,
+legend_name="Quantidade de Notificações",
+smooth_factor=0,
+Highlight= True,
+line_color = "#0000",
+name = "Quantidade de Notificações",
+show=False,
+overlay=True,
+nan_fill_color = "White"
+).add_to(map_notificacao)
+
+
+
+map_notificacao.save("map_notificacao.html")
+webbrowser.open("map_notificacao.html")
+############################# Embargo ###############################################################################
+print(geoJSON.columns)
+geoJSON
+
+############################# Interdição #############################################################################
 
 conexao.close()
-bairros_chave["Auto de Infração"] = auto_infracao
-print(bairros_chave.columns)
-geoJSON = gpd.read_file('C:/Users/EquipeDev/Desktop/faus_holiday/produtividade/fiscal_digital/bairros_arapiraca.geojson')
-print(bairros_chave.colulmns)
-print(geoJSON.columns)
 
 
-map = folium.Map(location=[-9.751969332753832, -36.656550027706835], zoom_start=12,  tiles='OpenStreetMap')
-
-
-
-
-
-map.save("map.html")
-webbrowser.open("map.html")
